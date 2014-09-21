a9fb5f1e4c2156ed831a884aef4c94e612c06f91
psirenny <djtorres0@gmail.com>
2014-09-21 11:53:57 -0400

    update to match newer version of derby-user

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-module.exports = function (app, options) {
-  if (!options) options = {};
-  if (!options.origin) options.origin = 'http://localhost:3000'
-
-  return {
-    init: require('./lib/init')(app, options),
-    routes: require('./lib/routes')(app, options)
-  }
-};
+module.exports = require('./lib');
